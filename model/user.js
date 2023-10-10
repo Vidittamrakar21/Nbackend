fe2ede008e0385ae538c53baee05f546ec0265aa
Vidit <vidit.tamrakar16@gmail.com>
2023-10-10 19:15:29 +0530

    update/o

---
+++ model/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const userschema = new Schema({
+
+    name: String,
+    email: String,
+    password: String,
+    followers: {type: Number, default:0},
+    postliked: {type: Array, default:[]},
+    blogposted: {type: Array, default:[]},
+    saved: {type: Array, default:[]},
+    about: {type: String, default:""},
+    token: String,
+    date: {type: Date, default: Date.now}
+    // image: String;
+
+
+})
+
+module.exports = mongoose.model('users',userschema);
--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const morgan = require('morgan');
 const { json } = require('stream/consumers');
 const mongoose = require('mongoose');
 const Blog = require('./model/blog');
+const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieparser = require('cookie-parser');
+require('dotenv').config();
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieparser());
 app.use(morgan("tiny"));
 
 main().catch(err => console.log(err));
@@ -92,6 +99,19 @@ app.get('/api/trend', async (req,res)=>{
   }
 })
 
+app.get('/api/search', async(req,res)=>{
+  try {
+    const {item } = req.body;
+    const searchTerm = item;
+    const regex = new RegExp(searchTerm, 'i');
+  const data = await Blog.find({title: regex})
+  res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error)
+  }
+})
+
 app.post('/api/post', async (req,res) =>{
   try {
     const blogdata = req.body;
@@ -137,6 +157,112 @@ app.delete('/', (req,res) =>{
     res.json({name:`hii ${name}`,message: "This is a delete test request"});
 })
 
+
+
+
+// user api's/////
+
+app.post('/api/register', async (req,res) =>{
+    try {
+
+      const {name ,email ,password} = req.body;
+      const existuser = await User.findOne({email});
+
+      if(!(name && email && password)){
+        res.status(200).json({message: "All the fields are required !"});
+      }
+
+      else if(existuser){
+        res.status(200).json({message: "User already exists !"});
+      }
+
+      else{
+
+        const hashpass = await bcrypt.hash(password , 10);
+
+        const token = jwt.sign(
+          {id: User._id, email ,name},
+          process.env.SECKEY,
+         {
+           expiresIn: "3h"
+         }
+        )
+        
+        const newuser = await User.create({
+          name,
+          email,
+          password:hashpass
+        })
+
+        newuser.token = token;
+        newuser.password = undefined;
+
+        res.status(200).send({info: newuser, message: "Account created successfuly!, Login to continue."})
+
+      }
+
+      
+      
+    } catch (error) {
+      res.status(200).json({message:"Error occured while creating an acount ! Try again later."})
+    }
+})
+
+
+app.post('/api/login', async (req,res)=>{
+  try {
+
+    const {  email , password} = req.body;
+    const existuser = await User.findOne({email});
+
+    if(!( email && password)){
+      res.status(200).json({message: "All the fields are required !"})
+    }
+
+    else if(!existuser){
+      res.status(200).json({message: "You don't have an account yet ! ,Kindly SignUp to continue."})
+    }
+
+    else{
+      if(existuser && (await bcrypt.compare(password, existuser.password))){
+
+        const token = jwt.sign(
+          {id: existuser._id, email, name: existuser.name},
+          process.env.SECKEY,
+          {
+            expiresIn: "3h"
+          }
+          )
+
+          existuser.token = token;
+          existuser.password = undefined;
+
+          const options = {
+            expires: new Date(Date.now() + 3*24*60*60*1000),
+            httpOnly: true
+          }
+
+          res.status(200).cookie("token" , token, options).json({
+            success: true,
+            token,
+            existuser,
+            message: "Logged In Successfully !"
+          })
+      }
+
+      else{
+        res.json({message: "Invalid email or password !"});
+      }
+    }
+    
+  } catch (error) {
+    res.status(200).json({message: "Error occured while logging ! Try again later."})
+  }
+})
+
+
+
+
 app.listen(8080, ()=> {
-    console.log("server started");
+  console.log("server started");
 })
